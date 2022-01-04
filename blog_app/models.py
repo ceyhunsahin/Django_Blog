0fa1e985a63312d2c96a474fed8b88aaf12be831
ceyhunsahin <csahin17072010@gmail.com>
2022-01-04 20:00:35 +0100

    blog_2

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}'.format(instance.author.id, filename)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    class Meta :
+        verbose_name_plural = "Categories"
+    def __str__(self):
+        return self.name
 # Create your models here.
+class Post(models.Model):
+    OPTIONS = (
+        ('drft', 'Draft'),
+        ('pblshd', 'published')
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to=user_directory_path, default='django.jpg')
+    category = models.ForeignKey(Category, on_delete = models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS,default='drft')
+    slug = models.SlugField(blank= True, unique=True)
+    def __str__(self):
+        return self.title
+
